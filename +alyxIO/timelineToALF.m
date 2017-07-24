92ecb688712e95ac02c57d3d712a59332818d566
Nick Steinmetz <nick.steinmetz@gmail.com>
2017-07-24 21:58:13 +0100

    add timelineToALF

---
+++ +alyxIO/timelineToALF.m
@@ -0,0 +1,43 @@
+
+
+function timelineToALF(Timeline, b, destDir)
+% function timelineToALF(timeline, b, destDir)
+%
+% Converts a Timeline structure to alyx file format
+%
+% Inputs:
+% - Timeline, a timeline structure
+% - b, a 2-element conversion vector to universal timebase, optional
+% - destDir, a place to put the results
+
+if ~exist('writeNPY')
+    error('writeNPY not found; cannot proceed saving Timeline to ALF');
+end
+
+% Save hw json
+if exist('savejson')
+    savejson('hw', Timeline.hw, fullfile(destDir, 'TimelineHW.json'));
+else
+    warning('Jsonlab not found - hardware information not saved to ALF')
+end
+
+
+% Save each recorded vector into the correct format in Timeline timebase
+% and optionally into universal timebase if conversion is provided
+inputs = {Timeline.hw.inputs.name};
+nSamps = Timeline.rawDAQSampleCount;
+tlTimes = [0 Timeline.rawDAQTimestamps(1); nSamps-1 Timeline.rawDAQTimestamps(end)];
+for ii = 1:length(inputs)
+    
+    dat = Timeline.rawDAQData(:,ii);
+    
+    writeNPY(dat, fullfile(destDir, [inputs{ii} '.npy']));
+    
+    writeNPY(tlTimes, fullfile(destDir, [inputs{ii} '.timestamps_Timeline.npy']));
+    
+    if ~isempty(b) && numel(b)==2
+        univTimes = [tlTimes(:,2) [1;1]]*b(:);
+        writeNPY(univTimes, fullfile(destDir, [inputs{ii} '.timestamps.npy']));
+    end
+    
+end

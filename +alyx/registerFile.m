8c294360ddade8fbe5f83832f011cdec08f3d038
k1o0 <k1o0@3tk.co>
2018-01-18 13:02:03 +0000

    Updated registerFile
    registerFile2 renamed as registerFile

--- +alyx/registerFile.m
+++ +alyx/registerFile.m
@@ -1,107 +1,91 @@
-function [dataset,filerecord] = registerFile(subject,subsessionURL,datasetType,fullPath,repository,alyxInstance)
-%[dataset,filerecord] = registerFile(subject,subsessionURL,datasetType,fullPath,repository,alyxInstance)
-% Registers a file to Alyx. This works first by creating a dataset (a record of the dataset type, creation date, md5 hash), and
-% then a filerecord (a record of the relative path of one instance of the
-% file in the repository). The dataset is associated with a subsession, which
-% can be provided or inferred from the creation date. If the subsession URL
-% is not provided, the code searches for sessions which have the TYPE field
-% as 'Experiment', therefore if you don't provide a URL, your subsessions must include this TYPE field entry. 
+function [dataset, filerecord] = registerFile(filePath, dataFormatName, sessionURL, datasetTypeName, parentDatasetURL, alyxInstance)
+%[dataset,filerecord] = registerFile2(filePath, dataFormatName, sessionURL, datasetTypeName, parentDatasetURL, alyxInstance)
+% Registers a filepath to Alyx. The file being registered should already be on the target server.
+% The repository being registered to will be automatically determined from
+% the filePath.
+% Registration work first by creating a dataset (a record of the dataset type, creation date, md5 hash), and
+% then a filerecord (a record of the relative path within the repository). The dataset is associated with a session, which
+% must be provided. Also must provide a datasetType. Can optionally provide
+% a parentDataSet URL and alyxInstance
 % Inputs:
-% -subject: 'Nyx', 'PC001', etc
-% -subsessionURL: Alyx URL for the subsession. If empty then we search the
-% latest one for the current date, errors if not found.
-% -datasetType: Block, Timeline, Parameters
-% -fullPath: the full path of the file on the server
-%   (e.g. '\\zserver.cortexlab.net\Data\...\blabla_Timeline.mat')
-% -repository: name of the remote repository the data is stored on (e.g.
-%   'zserver');
-% -AlyxInstance: An alyx instance object, created from alyx.loginWindow().
-%   If empty, then creates one.
+% -filePath: full path of the file being registered
+% -dataFormatName: data format, e.g. 'npy', 'mj2', or 'notData'
+% -sessionURL: Alyx URL for the session.
+% -datasetTypeName: Block, Timeline, Parameters, eye.movie, etc
+% -parentDatasetURL: optional URL for a parent dataset
+% -AlyxInstance: Optional alyx instance object, created from alyx.loginWindow().
 
-   
+%%INPUT VALIDATION
+%Validate input path
+assert( ~contains(filePath,'/'), 'Do not use forward slashes in the path');
+assert( exist(filePath,'file') == 2 , 'Path %s does not exist', filePath);
+assert( ~isdir(filePath), 'filePath supplied must not be a folder');
 
-if contains(fullPath,'/')
-    error('Use backslashes in path');
+%Validate alyxInstance, creating one if not supplied
+if isempty(alyxInstance); alyxInstance = alyx.loginWindow(); end
+assert(isfield(alyxInstance,'token'), 'Supplied alyxInstance is improper');
+
+%Validate dataFormat supplied
+dataFormats = alyx.getData(alyxInstance,['data-formats']);
+dataFormats = [dataFormats{:}];
+dataFormatIdx = find( strcmp({dataFormats.name}, dataFormatName) );
+assert( ~isempty(dataFormatIdx), 'dataFormat %s not found', dataFormatName);
+
+%Validate sessionURL supplied
+[status,body] = http.jsonGet(sessionURL, 'Authorization', ['Token ' alyxInstance.token]);
+assert(status==200,'SessionURL Invalid');
+
+%Validate optional parentDatasetURL
+if ~isempty(parentDatasetURL)
+    [status,body] = http.jsonGet(parentDatasetURL, 'Authorization', ['Token ' alyxInstance.token]);
+    assert(status==200,'parentDatasetURL Invalid');
 end
 
-info = dir(fullPath);
-created_time = alyx.datestr(info.datenum);
+%Validate datasetType supplied
+datasetTypes = alyx.getData(alyxInstance,'dataset-types'); 
+datasetTypes = [datasetTypes{:}];
+datasetTypeIdx = find( strcmp({datasetTypes.name},datasetTypeName) );
+assert( ~isempty(datasetTypeIdx), 'DatasetType %s not found', datasetTypeName);
 
-%Create alyx login instance if not already supplied
-if isempty(alyxInstance)
-    alyxInstance = alyx.loginWindow();
-    if isempty(alyxInstance) % login failed or cancelled
-        error('login failed');
-    end
-end
+%% Now some preparations
+%Get datarepositories and their base paths
+repositories = alyx.getData(alyxInstance, ['data-repository']);
+repo_paths = cellfun( @(r) r.path, repositories, 'uni', 0);
 
-if isempty(subsessionURL)
-    %If no subsession provided, then search for the latest one for the same
-    %file creation date 
-    subsessions = alyx.getData(alyxInstance, ['sessions?type=Experiment&subject=' subject]);
-    if isempty(subsessions)
-        error(['No subsessions found for subject ' subject]);
-    end
-    
-    latest_subsession = subsessions{end};
-    if ~strcmp(latest_subsession.start_time(1:10), created_time(1:10))
-        error('Latest subsession found in Alyx has a different creation date to the file being registered');
-    end
-    
-    subsessionURL = latest_subsession.url;
-end
+%Identify which repository the filePath is in
+which_repo = cellfun( @(rp) startsWith(filePath, rp), repo_paths);
+assert(sum(which_repo)==1, 'Input filePath\n%s\ndoes not contain the a repository path\n',filePath);
 
-%Get root directory mask of the repository
-datarepository = alyx.getData(alyxInstance, ['data-repository?name=' repository]);
-if isempty(datarepository)
-    error(['Data repository ' repository ' not found']);
-end
+%Define the relative path of the file within the repo
+relativePath = strrep(filePath,repo_paths{which_repo},'');
 
-if startsWith(fullPath,datarepository{1}.path)
-    relPath = strrep(fullPath,datarepository{1}.path,'');
-else
-    error(['File path does not contain the repository path signature: ' datarepository{1}.path]);
-end
-
-%Create dataset on Alyx, get the UUID for that dataset
-d=struct;
-d.created_by = alyxInstance.username;
-d.dataset_type = datasetType;
-d.session = subsessionURL;
-d.created_date = created_time;
-
+%% Now submit Dataset and Filerecord to Alyx
+pathInfo = dir(filePath); %Get path creation date/etc
+d = struct('created_by',alyxInstance.username,...
+    'dataset_type',datasetTypeName,...
+    'data_format',dataFormatName,...
+    'session',sessionURL,...
+    'created_date',alyx.datestr(pathInfo.datenum));
 try
-    if ~isdir(fullPath)
-        [~,name,extension] = fileparts(fullPath);
-        d.name = [name extension];
-        
-        d.md5 = mMD5(fullPath);
-    else
-        d.name = 'DIR';
-    end
+    d.md5 = mMD5(filePath);
 catch
-    warning(['Failed to compute file md5, the function mMD5 is included in this '...
-        'repository but you may need to compile it. Navigate to /alyx-matlab/'...
-        'helpers/ and use ''mex mMD5.mexw64''']);
+    warning('Failed to compute MD5, using NULL');
 end
 
-try
-    dataset = alyx.postData(alyxInstance, 'datasets', d);
-catch
-    error('Posting dataset failed');
+if ~isempty(parentDatasetURL)
+   d.parent_dataset = parentDatasetURL;
 end
 
+[datasetReturnData, statusCode] = alyx.postData(alyxInstance, 'datasets', d);
+assert(statusCode==201, 'Failed to submit dataset to Alyx');
 
-%Create filerecord, using the dataset just created
-d = struct;
-d.dataset = dataset.url;
-d.data_repository = repository;
-d.relative_path = relPath;
+d = struct('dataset', datasetReturnData.url,...
+    'data_repository', 'zserver',...
+    'relative_path', relativePath);
 
-try
-    filerecord = alyx.postData(alyxInstance, 'files', d);
-catch
-    error('posting filerecord failed');
-end
+[fileRecordReturnData, statusCode] = alyx.postData(alyxInstance, 'files', d);
+assert(statusCode==201, 'Failed to submit filerecord to Alyx');
 
+dataset = datasetReturnData;
+filerecord = fileRecordReturnData;
 end

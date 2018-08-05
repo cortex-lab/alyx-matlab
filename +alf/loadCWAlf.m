7ccc1b6488acbb3498c8cf0775ee0519d49607a9
nsteinme <nick.steinmetz@gmail.com>
2018-08-05 19:57:04 +0100

    update loadCWalf

--- +alf/loadCWAlf.m
+++ +alf/loadCWAlf.m
@@ -1,6 +1,6 @@
 
-function [sp, cweA, cwtA, moveData, lickTimes, incl] = loadCWAlf(mouseName, thisDate, ephysTag)
-% function [sp, cweA, cwtA, moveData, lickTimes, incl] = loadCWAlf(mouseName, thisDate, ephysTag)
+function [sp, cweA, cwtA, moveData, lickTimes, passiveStim] = loadCWAlf(mouseName, thisDate, ephysTag)
+% function [sp, cweA, cwtA, moveData, lickTimes, passiveStim] = loadCWAlf(mouseName, thisDate, ephysTag)
 %
 
  
@@ -49,6 +49,18 @@ else
         borders = table(upperBorder, lowerBorder, acronym);
     end
     
+    % identify acronym for each cluster
+%     acr = cell(size(cds)); 
+%     for q = 1:numel(acr)
+%         bInd = find(cds(q)>borders.lowerBorder & cds(q)<=borders.upperBorder);
+%         if ~isempty(bInd)
+%             acr{q} = borders.acronym{bInd}; 
+%         end
+%     end
+    [acr, d] = listInclArea(); st = loadStructureTree();
+    stAcr = arrayfun(@(x)standardAcrByDepth(borders, x, acr, d, st), cds, 'uni', false);
+    
+    sp.acr = stAcr;
     sp.coords = coords;
     sp.borders = borders;
     
@@ -77,11 +89,70 @@ moveOnsets = wheelMoves(:,1); moveOffsets = wheelMoves(:,2);
 moveType = readNPY(fullfile(alfDir, 'wheelMoves.type.npy'));
 moveData.moveOnsets = moveOnsets; moveData.moveOffsets = moveOffsets; moveData.moveType = moveType;
 
+% - wheel data, to include in moveData struct
+moveData.wheelPos = readNPY(fullfile(alfDir, 'wheel.position.npy'));
+moveData.wheelT = readNPY(fullfile(alfDir, 'wheel.timestamps.npy'));
+% moveData.wheelT = interp1(wt(:,1), wt(:,2), (0:(numel(moveData.wheelPos)-1))');
+
 % - lickTimes - a vector of lick times
 lickTimes = readNPY(fullfile(alfDir, 'licks.times.npy'));
 
-if exist(fullfile(alfDir, 'include.recording.npy'))
-    incl = readNPY(fullfile(alfDir, 'include.recording.npy'));
+
+
+%passive data
+ad = alfDir;
+if exist(fullfile(ad, 'passiveStimOn.times.npy'), 'file')
+    stimOn = readNPY(fullfile(ad, 'passiveStimOn.times.npy'));
+    contrastLeft = readNPY(fullfile(ad, 'passiveStimOn.contrastLeft.npy'));
+    contrastRight = readNPY(fullfile(ad, 'passiveStimOn.contrastRight.npy'));
+    valveClick = readNPY(fullfile(ad, 'passiveValveClick.times.npy'));
+    whiteNoise = readNPY(fullfile(ad, 'passiveWhiteNoise.times.npy'));
+    beep = readNPY(fullfile(ad, 'passiveBeep.times.npy'));
+    
+    isValveClick = false(size(stimOn));
+    isWhiteNoise = false(size(stimOn));
+    isBeep = false(size(stimOn));
+    
+    if ~isnan(valveClick(1))
+        stimOn = [stimOn; valveClick];
+        contrastLeft = [contrastLeft; zeros(size(valveClick))];
+        contrastRight = [contrastRight; zeros(size(valveClick))];
+        isValveClick = [isValveClick; true(size(valveClick))];
+        isWhiteNoise = [isWhiteNoise; false(size(valveClick))];
+        isBeep = [isBeep; false(size(valveClick))];
+    end
+    
+    if ~isnan(whiteNoise(1))
+        stimOn = [stimOn; whiteNoise];
+        contrastLeft = [contrastLeft; zeros(size(whiteNoise))];
+        contrastRight = [contrastRight; zeros(size(whiteNoise))];
+        isValveClick = [isValveClick; false(size(whiteNoise))];
+        isWhiteNoise = [isWhiteNoise; true(size(whiteNoise))];
+        isBeep = [isBeep; false(size(whiteNoise))];
+    end
+    
+    if ~isnan(beep(1))
+        stimOn = [stimOn; beep];
+        contrastLeft = [contrastLeft; zeros(size(beep))];
+        contrastRight = [contrastRight; zeros(size(beep))];
+        isValveClick = [isValveClick; false(size(beep))];
+        isWhiteNoise = [isWhiteNoise; false(size(beep))];
+        isBeep = [isBeep; true(size(beep))];
+    end
+    
 else
-    incl = [];
+    fprintf(1, 'passive not found\n')
+    stimOn = []; contrastLeft = []; contrastRight = [];
+    isValveClick = []; isWhiteNoise = []; isBeep = [];
 end
+
+passiveStim = table(stimOn, contrastLeft, contrastRight, isValveClick, isWhiteNoise, isBeep);
+passiveStim = sortrows(passiveStim, 'stimOn');
+
+
+
+% if exist(fullfile(alfDir, 'include.recording.npy'))
+%     incl = readNPY(fullfile(alfDir, 'include.recording.npy'));
+% else
+%     incl = [];
+% end
